d6d153d502736205ca60f48c9f23d88ef896cfa6
mwasilew <01158977@pw.edu.pl>
2023-04-18 18:24:39 +0200

    Parametrized all tests in LexerKeywordsTest

--- test/unit_test/LexerKeywordsTest.java
+++ test/unit_test/LexerKeywordsTest.java
@@ -1,234 +1,53 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class LexerKeywordsTest {
-    @Test
-    void lexIntKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Int".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
 
-        assertEquals(TokenTypeEnum.INT_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
+    @ParameterizedTest
+    @MethodSource("generateKeywordTokensData")
+    void lexIntValue(SingleTokenTestParams testScenarioParams) {
+        performTest(testScenarioParams);
     }
 
-    @Test
-    void lexDoubleKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Double".getBytes());
+    private static void performTest(SingleTokenTestParams testScenarioParams) {
+        InputStream inputStream = new ByteArrayInputStream(testScenarioParams.inputString().getBytes());
         BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
         Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
 
-        assertEquals(TokenTypeEnum.DOUBLE_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
+        Token token = lex.lexToken();
+        assertEquals(testScenarioParams.token().tokenType(), token.getTokenType());
         assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
+        assertEquals(testScenarioParams.token().lineNumber(), token.getPosition().getLineNumber());
+        assertEquals(testScenarioParams.token().columnNumber(), token.getPosition().getColumnNumber());
     }
 
-    @Test
-    void lexStringKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("String".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.STRING_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexPointKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Point".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.POINT_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexSectionKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Section".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.SECTION_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexFigureKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Figure".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.FIGURE_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexSceneKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Scene".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.SCENE_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexBoolKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("Bool".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.BOOL_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexListKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("List".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.LIST_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexWhileKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("while".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.WHILE_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexIfKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("if".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.IF_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexElseIfKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("elseif".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.ELSE_IF_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexElseKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("else".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.ELSE_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexMainKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("main".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.MAIN_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexReturnKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("return".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.RETURN_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
-    }
-
-    @Test
-    void lexVoidKeyword() {
-        InputStream inputStream = new ByteArrayInputStream("void".getBytes());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-        Lexer lex = new Lexer(bufferedInputStream);
-        Token token = lex.lexToken();
-
-        assertEquals(TokenTypeEnum.VOID_KEYWORD, token.getTokenType());
-        assertEquals(StringToken.class, token.getClass());
-        assertNull(token.getValue());
-        assertEquals(1, token.getPosition().getLineNumber());
-        assertEquals(1, token.getPosition().getColumnNumber());
+    static Stream<Arguments> generateKeywordTokensData() {
+        return Stream.of(
+                Arguments.of(new SingleTokenTestParams("Int", new SingleTokenDescription(TokenTypeEnum.INT_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("Double", new SingleTokenDescription(TokenTypeEnum.DOUBLE_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("String", new SingleTokenDescription(TokenTypeEnum.STRING_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("Point", new SingleTokenDescription(TokenTypeEnum.POINT_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("Section", new SingleTokenDescription(TokenTypeEnum.SECTION_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("Figure", new SingleTokenDescription(TokenTypeEnum.FIGURE_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("Scene", new SingleTokenDescription(TokenTypeEnum.SCENE_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("Bool", new SingleTokenDescription(TokenTypeEnum.BOOL_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("List", new SingleTokenDescription(TokenTypeEnum.LIST_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("while", new SingleTokenDescription(TokenTypeEnum.WHILE_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("if", new SingleTokenDescription(TokenTypeEnum.IF_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("elseif", new SingleTokenDescription(TokenTypeEnum.ELSE_IF_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("else", new SingleTokenDescription(TokenTypeEnum.ELSE_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("main", new SingleTokenDescription(TokenTypeEnum.MAIN_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("return", new SingleTokenDescription(TokenTypeEnum.RETURN_KEYWORD, null, 1, 1))),
+                Arguments.of(new SingleTokenTestParams("void", new SingleTokenDescription(TokenTypeEnum.VOID_KEYWORD, null, 1, 1)))
+        );
     }
 }
