f8e1a5c4de7cab5a88e3411cfd276ab42a52e357
mwasilew <01158977@pw.edu.pl>
2023-05-02 12:15:51 +0200

    Parametrized ParserTest

--- test/parser/unit_test/ParserTest.java
+++ test/parser/unit_test/ParserTest.java
@@ -3,15 +3,20 @@ import lexer.tokens.StringToken;
 import lexer.tokens.Token;
 import lexer.TokenTypeEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import parser.Parser;
 import parser.program_components.Program;
 import parser.program_components.FunctionDef;
 import parser.program_components.BlockStatement;
 import utils.MockedLexer;
+import utils.ParserSingleTestParams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,32 +31,39 @@ public class ParserTest {
         assertNotNull(parser);
     }
 
-    @Test
-    void parseEmptyTokenList() {
-        ArrayList<Token> tokens = new ArrayList<>();
+    @ParameterizedTest
+    @MethodSource("generateTestProgramData")
+    void parseProgram(ParserSingleTestParams testParams) {
+        ArrayList<Token> tokens = new ArrayList<>(testParams.tokens());
         Parser parser = new Parser(new MockedLexer(tokens));
         Program program = parser.parse();
 
-        assertEquals(new HashMap<String, FunctionDef>(), program.functions());
+        assertEquals(testParams.expectedFunctions(), program.functions());
     }
 
-    @Test
-    void parseOnlyMainIdentifier() {
-        ArrayList<Token> tokens = new ArrayList<>(
-                Arrays.asList(
-                        new Token(new Position(1, 1), TokenTypeEnum.INT_KEYWORD),
-                        new StringToken("main", new Position(1, 5), TokenTypeEnum.IDENTIFIER),
-                        new Token(new Position(1, 9), TokenTypeEnum.LEFT_BRACKET),
-                        new Token(new Position(1, 10), TokenTypeEnum.RIGHT_BRACKET),
-                        new Token(new Position(1, 11), TokenTypeEnum.LEFT_CURLY_BRACKET),
-                        new Token(new Position(1, 12), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+    static Stream<Arguments> generateTestProgramData() {
+        return Stream.of(
+                Arguments.of(
+                        new ParserSingleTestParams(
+                                new ArrayList<>(),
+                                new HashMap<>()
                         )
+                ),
+                Arguments.of(
+                        new ParserSingleTestParams(
+                                Arrays.asList(
+                                        new Token(new Position(1, 1), TokenTypeEnum.INT_KEYWORD),
+                                        new StringToken("main", new Position(1, 5), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(1, 9), TokenTypeEnum.LEFT_BRACKET),
+                                        new Token(new Position(1, 10), TokenTypeEnum.RIGHT_BRACKET),
+                                        new Token(new Position(1, 11), TokenTypeEnum.LEFT_CURLY_BRACKET),
+                                        new Token(new Position(1, 12), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+                                ),
+                                new HashMap<>() {{
+                                    put("main", new FunctionDef("main", TokenTypeEnum.INT_KEYWORD, new ArrayList<>(), new BlockStatement(new ArrayList<>())));
+                                }}
+                        )
+                )
         );
-        Parser parser = new Parser(new MockedLexer(tokens));
-        Program program = parser.parse();
-        HashMap<String, FunctionDef> expectedFunctions = new HashMap<>();
-        expectedFunctions.put("main", new FunctionDef("main", TokenTypeEnum.INT_KEYWORD, new ArrayList<>(), new BlockStatement(new ArrayList<>())));
-
-        assertEquals(expectedFunctions, program.functions());
     }
 }
---
+++ test/parser/utils/ParserSingleTestParams.java
@@ -0,0 +1,10 @@
+package utils;
+
+import lexer.tokens.Token;
+import parser.program_components.FunctionDef;
+
+import java.util.HashMap;
+import java.util.List;
+
+public record ParserSingleTestParams(List<Token> tokens, HashMap<String, FunctionDef> expectedFunctions) {
+}
