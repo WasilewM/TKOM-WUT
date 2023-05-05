469cffd553d810e0a9dba029cb282bef8b116fee
mwasilew <01158977@pw.edu.pl>
2023-05-05 10:19:24 +0200

    Parametrized malformed if expression parsing tests

--- test/parser/unit_test/ParserMalformedIfExpressionTest.java
+++ test/parser/unit_test/ParserMalformedIfExpressionTest.java
@@ -3,23 +3,54 @@ import lexer.TokenTypeEnum;
 import lexer.tokens.StringToken;
 import lexer.tokens.Token;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import parser.Parser;
 import parser.exceptions.MissingLeftBracketException;
 import parser.exceptions.MissingRightBracketException;
 import utils.MockedExitErrorHandler;
 import utils.MockedLexer;
+import utils.ParserMalformedSingleTestParams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserMalformedIfExpressionTest {
 
     private ArrayList<Token> startTokens;
+
+    public static Stream<Arguments> getMalformedIfExpression_withHandleableExceptions() {
+        return Stream.of(
+                Arguments.of(
+                        new ParserMalformedSingleTestParams(
+                                List.of(
+                                        new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+                                ),
+                                Arrays.asList(
+                                        new MissingLeftBracketException(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET).toString()),
+                                        new MissingRightBracketException(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET).toString())
+                                )
+                        )
+                ),
+                Arguments.of(
+                        new ParserMalformedSingleTestParams(
+                                Arrays.asList(
+                                        new Token(new Position(10, 1), TokenTypeEnum.LEFT_BRACKET),
+                                        new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+                                ),
+                                List.of(
+                                        new MissingRightBracketException(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET).toString())
+                                )
+                        )
+                )
+        );
+    }
 
     @BeforeEach
     public void initTestParams() {
@@ -35,46 +66,20 @@ public class ParserMalformedIfExpressionTest {
         );
     }
 
-    @Test
-    void parseIfExpressionWithMissingLeftBracket() {
+    @ParameterizedTest
+    @MethodSource("getMalformedIfExpression_withHandleableExceptions")
+    void parseMalformedIfExpression_withHandleableExceptions(ParserMalformedSingleTestParams additionalParams) {
         ArrayList<Token> testTokens = new ArrayList<>(startTokens);
-        testTokens.add(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET));
-        List<Exception> expectedErrorLog = Arrays.asList(
-                new MissingLeftBracketException(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET).toString()),
-                new MissingRightBracketException(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET).toString())
-        );
-
+        testTokens.addAll(additionalParams.tokens());
         MockedExitErrorHandler errorHandler = new MockedExitErrorHandler();
         Parser parser = new Parser(new MockedLexer(testTokens), errorHandler);
         parser.parse();
 
-        Iterator<Exception> expected = expectedErrorLog.iterator();
+        Iterator<Exception> expected = additionalParams.expectedErrorLog().iterator();
         Iterator<Exception> actual = errorHandler.getErrorLog().iterator();
-        assertEquals(expectedErrorLog.size(), errorHandler.getErrorLog().size());
+        assertEquals(additionalParams.expectedErrorLog().size(), errorHandler.getErrorLog().size());
         while (expected.hasNext() && actual.hasNext()) {
             assertEquals(expected.next().getMessage(), actual.next().getMessage());
         }
     }
-
-    @Test
-    void parseIfExpressionWithMissingRightBracket() {
-        ArrayList<Token> testTokens = new ArrayList<>(startTokens);
-        testTokens.add(new Token(new Position(10, 1), TokenTypeEnum.LEFT_BRACKET));
-        testTokens.add(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET));
-        List<Exception> expectedErrorLog = List.of(
-                new MissingRightBracketException(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET).toString())
-        );
-
-        MockedExitErrorHandler errorHandler = new MockedExitErrorHandler();
-        Parser parser = new Parser(new MockedLexer(testTokens), errorHandler);
-        parser.parse();
-
-        Iterator<Exception> expected = expectedErrorLog.iterator();
-        Iterator<Exception> actual = errorHandler.getErrorLog().iterator();
-        assertEquals(expectedErrorLog.size(), errorHandler.getErrorLog().size());
-        while (expected.hasNext() && actual.hasNext()) {
-            assertEquals(expected.next().getMessage(), actual.next().getMessage());
-        }
-    }
-
 }
