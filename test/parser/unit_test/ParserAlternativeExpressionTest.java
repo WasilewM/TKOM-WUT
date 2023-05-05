0ecfdaf3a0733c740528227599ba0b8f96e63089
mwasilew <01158977@pw.edu.pl>
2023-05-05 09:10:24 +0200

    Parametrized alternative expression parsing tests

--- test/parser/unit_test/ParserAlternativeExpressionTest.java
+++ test/parser/unit_test/ParserAlternativeExpressionTest.java
@@ -3,22 +3,73 @@ import lexer.TokenTypeEnum;
 import lexer.tokens.StringToken;
 import lexer.tokens.Token;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import parser.Parser;
 import parser.program_components.*;
 import utils.MockedExitErrorHandler;
 import utils.MockedLexer;
+import utils.ParserSingleTestParams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserAlternativeExpressionTest {
 
     private ArrayList<Token> startTokens;
+
+    static Stream<Arguments> getAlternativeExpTestData() {
+        return Stream.of(
+                Arguments.of(
+                        new ParserSingleTestParams(
+                                Arrays.asList(
+                                        new StringToken("ident", new Position(2, 5), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(50, 1), TokenTypeEnum.RIGHT_BRACKET),
+                                        new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+                                ),
+                                new HashMap<>() {{
+                                    put("func", new FunctionDef("func", TokenTypeEnum.INT_KEYWORD, new HashMap<>(), new CodeBlock(List.of(new IfExpression(new Identifier("ident"))))));
+                                }}
+                        )
+                ),
+                Arguments.of(
+                        new ParserSingleTestParams(
+                                Arrays.asList(
+                                        new StringToken("ident", new Position(2, 5), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(2, 12), TokenTypeEnum.OR_OPERATOR),
+                                        new StringToken("ident", new Position(2, 15), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(50, 1), TokenTypeEnum.RIGHT_BRACKET),
+                                        new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+                                ),
+                                new HashMap<>() {{
+                                    put("func", new FunctionDef("func", TokenTypeEnum.INT_KEYWORD, new HashMap<>(), new CodeBlock(List.of(new IfExpression(new AlternativeExpression(new Identifier("ident"), new Identifier("ident")))))));
+                                }}
+                        )
+                ),
+                Arguments.of(
+                        new ParserSingleTestParams(
+                                Arrays.asList(
+                                        new StringToken("ident", new Position(2, 5), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(2, 12), TokenTypeEnum.OR_OPERATOR),
+                                        new StringToken("ident", new Position(2, 15), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(2, 21), TokenTypeEnum.OR_OPERATOR),
+                                        new StringToken("ident", new Position(2, 24), TokenTypeEnum.IDENTIFIER),
+                                        new Token(new Position(50, 1), TokenTypeEnum.RIGHT_BRACKET),
+                                        new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET)
+                                ),
+                                new HashMap<>() {{
+                                    put("func", new FunctionDef("func", TokenTypeEnum.INT_KEYWORD, new HashMap<>(), new CodeBlock(List.of(new IfExpression(new AlternativeExpression(new AlternativeExpression(new Identifier("ident"), new Identifier("ident")), new Identifier("ident")))))));
+                                }}
+                        )
+                )
+        );
+    }
 
     @BeforeEach
     public void initTestParams() {
@@ -35,66 +86,17 @@ public class ParserAlternativeExpressionTest {
         );
     }
 
-    @Test
-    void parseAlternativeExpression_onlyLeftExp() {
+    @ParameterizedTest
+    @MethodSource("getAlternativeExpTestData")
+    void parseAlternativeExpression(ParserSingleTestParams additionalTestParams) {
         ArrayList<Token> testTokens = new ArrayList<>(startTokens);
-
-        testTokens.add(new StringToken("ident", new Position(2, 5), TokenTypeEnum.IDENTIFIER));
-        testTokens.add(new Token(new Position(50, 1), TokenTypeEnum.RIGHT_BRACKET));
-        testTokens.add(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET));
-        HashMap<String, FunctionDef> expectedFunctions = new HashMap<>() {{
-            put("func", new FunctionDef("func", TokenTypeEnum.INT_KEYWORD, new HashMap<>(), new CodeBlock(List.of(new IfExpression(new Identifier("ident"))))));
-        }};
+        testTokens.addAll(additionalTestParams.tokens());
 
         MockedExitErrorHandler errorHandler = new MockedExitErrorHandler();
         Parser parser = new Parser(new MockedLexer(testTokens), errorHandler);
         Program program = parser.parse();
 
-        assertEquals(expectedFunctions, program.functions());
-        assertEquals(0, errorHandler.getErrorLog().size());
-    }
-
-    @Test
-    void parseAlternativeExpression_leftAndRightExp() {
-        ArrayList<Token> testTokens = new ArrayList<>(startTokens);
-
-        testTokens.add(new StringToken("ident", new Position(2, 5), TokenTypeEnum.IDENTIFIER));
-        testTokens.add(new Token(new Position(2, 12), TokenTypeEnum.OR_OPERATOR));
-        testTokens.add(new StringToken("ident", new Position(2, 15), TokenTypeEnum.IDENTIFIER));
-        testTokens.add(new Token(new Position(50, 1), TokenTypeEnum.RIGHT_BRACKET));
-        testTokens.add(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET));
-        HashMap<String, FunctionDef> expectedFunctions = new HashMap<>() {{
-            put("func", new FunctionDef("func", TokenTypeEnum.INT_KEYWORD, new HashMap<>(), new CodeBlock(List.of(new IfExpression(new AlternativeExpression(new Identifier("ident"), new Identifier("ident")))))));
-        }};
-
-        MockedExitErrorHandler errorHandler = new MockedExitErrorHandler();
-        Parser parser = new Parser(new MockedLexer(testTokens), errorHandler);
-        Program program = parser.parse();
-
-        assertEquals(expectedFunctions, program.functions());
-        assertEquals(0, errorHandler.getErrorLog().size());
-    }
-
-    @Test
-    void parseAlternativeExpression_whenMultipleRightExp() {
-        ArrayList<Token> testTokens = new ArrayList<>(startTokens);
-
-        testTokens.add(new StringToken("ident", new Position(2, 5), TokenTypeEnum.IDENTIFIER));
-        testTokens.add(new Token(new Position(2, 12), TokenTypeEnum.OR_OPERATOR));
-        testTokens.add(new StringToken("ident", new Position(2, 15), TokenTypeEnum.IDENTIFIER));
-        testTokens.add(new Token(new Position(2, 21), TokenTypeEnum.OR_OPERATOR));
-        testTokens.add(new StringToken("ident", new Position(2, 24), TokenTypeEnum.IDENTIFIER));
-        testTokens.add(new Token(new Position(50, 1), TokenTypeEnum.RIGHT_BRACKET));
-        testTokens.add(new Token(new Position(100, 1), TokenTypeEnum.RIGHT_CURLY_BRACKET));
-        HashMap<String, FunctionDef> expectedFunctions = new HashMap<>() {{
-            put("func", new FunctionDef("func", TokenTypeEnum.INT_KEYWORD, new HashMap<>(), new CodeBlock(List.of(new IfExpression(new AlternativeExpression(new AlternativeExpression(new Identifier("ident"), new Identifier("ident")), new Identifier("ident")))))));
-        }};
-
-        MockedExitErrorHandler errorHandler = new MockedExitErrorHandler();
-        Parser parser = new Parser(new MockedLexer(testTokens), errorHandler);
-        Program program = parser.parse();
-
-        assertEquals(expectedFunctions, program.functions());
+        assertEquals(additionalTestParams.expectedFunctions(), program.functions());
         assertEquals(0, errorHandler.getErrorLog().size());
     }
 }
