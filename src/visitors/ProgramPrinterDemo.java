b17e4f1a21c5dd1187836a88924f108b7bfc4df1
mwasilew <01158977@pw.edu.pl>
2023-05-31 12:22:56 +0200

    Updated example.surface and created separate demo for Interpreter

--- src/visitors/ProgramPrinterDemo.java
+++ src/visitors/ProgramPrinterDemo.java
@@ -9,7 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-public class Main {
+public class ProgramPrinterDemo {
 
     public static void main(String[] args) throws FileNotFoundException {
         String filename = "src/visitors/example.surface";
